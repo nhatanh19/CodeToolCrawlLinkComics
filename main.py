11b4ed4b2d834c6a53a4db5774514891a762b294
Nhật Ánh <nhatanh19a10@gmail.com>
2024-02-29 14:07:29 +0700

    update lan 2 - hoan thien so bo

--- main.py
+++ main.py
@@ -37,42 +37,41 @@ def get_image_links(comic_links, chapter_number):
     # Mở trang web
     driver.get(comic_links)
 
-    # Lấy mã nguồn HTML của trang web đã mở
-    html_content = driver.page_source
+    # Số lượng trang bạn muốn lặp qua
+    num_pages = 35
 
-    # Đóng trình duyệt
-    driver.quit()
-
-    # Sử dụng BeautifulSoup để phân tích cú pháp HTML
-    soup = BeautifulSoup(html_content, 'html.parser')
-
-    # Tìm tất cả các phần tử <img> trong trang web
-    img_tags = soup.find_all('img')
-
-    # Tạo một danh sách để lưu trữ các URL hình ảnh
+    # Danh sách để lưu trữ tất cả các URL hình ảnh
     image_urls = []
 
-    # Lặp qua các phần tử <img> và lấy ra thuộc tính 'src' của chúng
-    for img in img_tags:
-        src = img.get('src')
-        if src and src.startswith(urlSearch):
-            image_urls.append(src)
+    # Lặp qua các trang
+    for i in range(1, num_pages + 1):
+        # Lấy các phần tử <img> với XPath tương ứng
+        img_elements = driver.find_elements(By.XPATH, f'//*[@id="page_{i}"]/img')
+        # Lặp qua từng phần tử <img> và lấy thuộc tính 'src'
+        for img in img_elements:
+            src = img.get_attribute('src')
+            if src:
+                image_urls.append(src)
+
+    # Đóng trình duyệt sau khi hoàn thành
+    driver.quit()
+
     file_name = f"Chapter {chapter_number}.txt"
     try:
         # Ghi các URL hình ảnh vào tệp văn bản
         with open(file_name, 'w') as file:
             for url in image_urls:
-                file.write("https:" + url + '\n')
+                file.write(url + '\n')
         print(f"Image URLs saved to {file_name}")
     except Exception as e:
         print(f"Error occurred while saving image URLs: {str(e)}")
 
 
 # URL của trang web chứa các chương truyện
-#comic_url = input("Nhập URL của trang web chứa truyện: ")
-#urlSearch = input("Nhập URL tìm kiếm link ảnh: ")
-comic_url = "https://www.nettruyenbb.com/truyen-tranh/du-bao-khai-huyen-100000"
-urlSearch = "//i32.ntcdntempv26.com/data/images/"
+comic_url = input("Nhập URL của trang web chứa truyện: ")
+urlSearch = input("Nhập URL tìm kiếm link ảnh: ")
+#comic_url = "https://www.nettruyenbb.com/truyen-tranh/du-bao-khai-huyen-100000"
+#urlSearch = "//i32.ntcdntempv26.com/data/images/"
 comic_links = get_comic_links(comic_url)
 
 if comic_links:
